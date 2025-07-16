5ea90106a28415c3fd1c362906e93efb07603a0a
Sachin3439 <nabajyotirout12@gmail.com>
2025-07-16 11:40:33 +0530

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -11,7 +11,7 @@ const otpStore = {};
 
 const app = express();
 app.use(cors({
-  origin: [ 'https://fitlife1-f6t1.vercel.app'],
+  origin: [ 'https://fitlife1.vercel.app'],
   methods:["POST","GET"],
   credentials:true
 }));
